4d48783725635d13248957bbabe10a4797e6c29e
chriswei01 <chris.wei@gatech.edu>
2021-10-13 11:05:30 -0400

    initial project upload

---
+++ app/src/main/java/com/example/recyclerviewdemo/NoteListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recyclerviewdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NoteListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
